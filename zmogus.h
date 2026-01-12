4d8da6256670b776fa3c45bc3c56c3a0362c1695
dirzinskas <mantasdirzinskas@gmail.com>
2026-01-12 20:35:00 +0200

    Add files via upload

---
+++ zmogus.h
@@ -0,0 +1,28 @@
+#ifndef ZMOGUS_H
+#define ZMOGUS_H
+
+#include <string>
+
+//pradesiu vartoti std:: vietoj namespace std  
+
+class Zmogus {
+protected:
+    std::string vardas;
+    std::string pavarde;
+    
+public:
+    Zmogus(const std::string& v = "", const std::string& p = "")
+        : vardas(v), pavarde(p) {}
+    
+    virtual ~Zmogus() = default;
+    
+    virtual void printInfo() const = 0;
+    
+    std::string getVardas() const { return vardas; }
+    std::string getPavarde() const { return pavarde; }
+    
+    void setVardas(const std::string& v) { vardas = v; }
+    void setPavarde(const std::string& p) { pavarde = p; }
+};
+
+#endif
